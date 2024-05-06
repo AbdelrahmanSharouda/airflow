1a3d703663b218f6c9c7c0ae03a7a19e9bf93b65
Abdelrahman Shawki <abdelrahman.shawki@talentsarena.net>
2024-05-06 23:11:06 +0300

    added python operator that runs a newly added method called greet

--- dags/airflow_trying.py
+++ dags/airflow_trying.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime,timedelta
 
 default_args = {
@@ -8,8 +9,11 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
+def greet():
+    print('Hello World!')
+
 with DAG(
-    dag_id='our_first_dag_v3',
+    dag_id='our_first_dag_v4',
     default_args=default_args,
     description='This is our first dag',
     start_date=datetime(2024, 5, 6, 2),
@@ -31,5 +35,10 @@ with DAG(
         bash_command="echo hey, running in the samme time as task2"
     )
     
+    task4 = PythonOperator(
+        task_id='fourth_task',
+        python_callable=greet
+    )
+    
     task1 >> task2
-    task1 >> task3
+    task1 >> task3 >> task4
