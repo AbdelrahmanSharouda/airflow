f6c7c1ef1750fea8311aaaef4d1579237d7190be
Abdelrahman Shawki <abdelrahman.shawki@talentsarena.net>
2024-05-06 18:44:43 +0300

    Added default_args in dags/airflow_trying.py

---
+++ dags/airflow_trying.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from datetime import datetime,timedelta
+
+default_args = {
+    'owner': 'coder2j',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='our_first_dag',
+    default_args=default_args,
+    description='This is our first dag',
+    start_date=datetime(2024, 5, 6, 2),
+    schedule_interval='@daily'
+    
+) as dag:
+    pass
